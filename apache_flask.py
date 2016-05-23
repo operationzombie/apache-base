285c5671dc7ea3045e2704d9dca674dca0b2be2b
Miten Chauhan <mitenchauhan@hotmail.com>
2016-05-23 12:49:19 +1200

    Attempting to add serial communications

--- apache_flask.py
+++ apache_flask.py
@@ -5,7 +5,22 @@
 	:license: MIT, see LICENSE for more details.
 """
 
+import time
+import serial
+
 from flask import Flask, make_response, render_template, jsonify
+
+ser = serial.Serial(
+    port='/dev/ttyUSB1',
+    baudrate=9600,
+    parity=serial.PARITY_ODD,
+    stopbits=serial.STOPBITS_TWO,
+    bytesize=serial.SEVENBITS
+)
+ser.isOpen()
+
+
+
 
 app = Flask(__name__)
 
@@ -14,7 +29,33 @@ from commontools import log
 #-----------------------------------
 @app.route('/')
 def index():
+	print 'Enter your commands below.\r\nInsert "exit" to leave the application.'
+
+	input=1
+	while 1 :
+		# get keyboard input
+		input = raw_input(">> ")
+		if input == 'exit':
+			ser.close()
+			exit()
+		else:
+			# send the character to the device
+			# (note that I happend a \r\n carriage return and line feed to the characters - this is requested by my device)
+			ser.write(input + '\r\n')
+			out = ''
+			# let's wait one second before reading output (let's give device time to answer)
+			time.sleep(1)
+			while ser.inWaiting() > 0:
+				out += ser.read(1)
+
+			if out != '':
+				print ">>" + out
 	return render_template('index.html')
+	
+#-----------------------------------
+@app.route("/", method=("POST",))
+def handle_data():
+    return "Hello World - you sent me " + str(request.values)
 
 #-----------------------------------
 @app.errorhandler(500)
