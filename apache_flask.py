6ac80d517a61c92dbe723628243c8f526d4883cf
mcking49 <mitenchauhan@hotmail.com>
2016-07-17 21:24:21 +1200

    Ping Ports Docker Test Testing if read/write to serial port works viz docker

--- apache_flask.py
+++ apache_flask.py
@@ -6,15 +6,28 @@
 """
 
 from flask import Flask, make_response, render_template, jsonify
+from flask import request
 
 app = Flask(__name__)
 
 from commontools import log
 
+import serial, time
+
 #-----------------------------------
-@app.route('/')
-def index():
-	return render_template('index.html')
+@app.route('/', methods=['POST', 'GET'])
+def index():	
+	if request.method == 'POST':
+		if request.form['submit'] == 'Do Something':
+			print('\n')
+			print '***************** PING MITEN ******************'
+			print('\n')
+			ser = serial.Serial('COM1')
+			ser.write('MITEN')
+		else:
+			pass # unknown
+	elif request.method == 'GET':
+		return render_template('index.html')
 
 #-----------------------------------
 @app.errorhandler(500)
@@ -27,3 +40,24 @@ def internal_error(error):
 @app.errorhandler(404)
 def not_found(error):
 	return make_response(jsonify( { 'error': 'Not found' } ), 404)
+
+def index():
+	if request.method == 'POST':
+		if request.form['submit'] == 'Do Something':
+			print('\n')
+			print '***************** PING MITEN ******************'
+			print('\n')
+			ser = serial.Serial('COM1')
+			ser.write('MITEN')
+		elif request.form['submit'] == 'Do Something Else':
+			print('\n')
+			print '***************** PING MITEN ******************'
+			print('\n')
+			ser = serial.Serial('COM1')
+			ser.write('MITEN')
+		else:
+			pass # unknown
+	elif request.method == 'GET':
+		return render_template('index.html')
+	
+
