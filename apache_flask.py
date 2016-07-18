a8fa5aea8d317e9534ad890e9b73f70ca9bc7818
Miten Chauhan <mitenchauhan@hotmail.com>
2016-07-18 15:35:29 +1200

    Master working code

--- apache_flask.py
+++ apache_flask.py
@@ -12,22 +12,10 @@ app = Flask(__name__)
 
 from commontools import log
 
-import serial, time
-
 #-----------------------------------
 @app.route('/', methods=['POST', 'GET'])
-def index():	
-	if request.method == 'POST':
-		if request.form['submit'] == 'Do Something':
-			print('\n')
-			print '***************** PING MITEN ******************'
-			print('\n')
-			ser = serial.Serial('COM1')
-			ser.write('MITEN')
-		else:
-			pass # unknown
-	elif request.method == 'GET':
-		return render_template('index.html')
+def index():
+	return render_template('index.html')
 
 #-----------------------------------
 @app.errorhandler(500)
@@ -42,22 +30,6 @@ def not_found(error):
 	return make_response(jsonify( { 'error': 'Not found' } ), 404)
 
 def index():
-	if request.method == 'POST':
-		if request.form['submit'] == 'Do Something':
-			print('\n')
-			print '***************** PING MITEN ******************'
-			print('\n')
-			ser = serial.Serial('COM1')
-			ser.write('MITEN')
-		elif request.form['submit'] == 'Do Something Else':
-			print('\n')
-			print '***************** PING MITEN ******************'
-			print('\n')
-			ser = serial.Serial('COM1')
-			ser.write('MITEN')
-		else:
-			pass # unknown
-	elif request.method == 'GET':
-		return render_template('index.html')
+	return render_template('index.html')
 	
 
