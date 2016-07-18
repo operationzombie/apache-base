13384a362cb09f045bc116af2728000f51a26937
Miten Chauhan <mitenchauhan@hotmail.com>
2016-07-18 15:39:32 +1200

    reset branch

--- apache_flask.py
+++ apache_flask.py
@@ -10,7 +10,7 @@ from flask import Flask, make_response, render_template, jsonify
 app = Flask(__name__)
 
 from commontools import log
-import serial, time
+#import serial, time
 #-----------------------------------
 @app.route('/')#, methods=['POST', 'GET'])
 def index():	
@@ -37,3 +37,17 @@ def internal_error(error):
 @app.errorhandler(404)
 def not_found(error):
 	return make_response(jsonify( { 'error': 'Not found' } ), 404)
+
+
+def index():	
+	"""if request.method == 'POST':
+		if request.form['submit'] == 'Do Something':
+			print('\n')
+			print '***************** PING MITEN ******************'
+			print('\n')
+			ser = serial.Serial('COM1')
+			ser.write('MITEN')
+		else:
+			pass # unknown
+	elif request.method == 'GET':"""
+	return render_template('index.html')
