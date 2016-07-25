82a8653cd9aa332e42a2358d801115c3d3d08eb9
CallumGill <callum.gill001@gmail.com>
2016-07-25 16:31:29 +1200

    Ping-pong working locally completely
    Docker implimentation is all that is now required

--- apache_flask.py
+++ apache_flask.py
@@ -5,16 +5,47 @@
 	:license: MIT, see LICENSE for more details.
 """
 
-from flask import Flask, make_response, render_template, jsonify
+from flask import Flask, make_response, render_template, jsonify, request
+
+import serial
 
 app = Flask(__name__)
 
 from commontools import log
 
 #-----------------------------------
-@app.route('/')
-def index():
-	return render_template('index.html')
+@app.route('/', methods=['POST', 'GET'])
+def index():    
+    if request.method == 'POST':
+        if request.form['submit'] == 'Ping':
+            print('\n')
+            print '***************** PINGING ******************'
+            print('\n')
+            ser = serial.Serial('COM1')
+            ser.write('PING')
+            print 'Pinged'
+            return render_template('index.html')
+        elif request.form['submit'] == 'Pong':
+       		print('\n')
+        	print '***************** WAITING FOR PONG ******************'
+        	print('\n')
+      		serRead = serial.Serial('COM2')
+        	recievedText = serRead.read(4)
+        	print recievedText
+        	print '******* PONGED ********'
+        	return render_template('index.html')
+    	elif request.form['submit'] == 'Ping-Pong':
+       		print('\n')
+        	print '***************** Ping-Pong ******************'
+        	print('\n')
+       		ser = serial.Serial('COM1')
+       		print 'WRITE PING'
+       		ser.write('PING')
+        	recievedText = ser.read(4)
+        	print recievedText
+        	return render_template('index.html')
+    elif request.method == 'GET':
+        return render_template('index.html')
 
 #-----------------------------------
 @app.errorhandler(500)
