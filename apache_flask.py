347be9f03b848bf87643b6153b92133f4135b061
CallumGill <callum.gill001@gmail.com>
2016-07-29 17:06:25 +1200

    Server can now read from socket within docker
    Still having trouble writing

--- apache_flask.py
+++ apache_flask.py
@@ -7,45 +7,60 @@
 
 from flask import Flask, make_response, render_template, jsonify, request
 
-import serial
+import serial, socket
 
 app = Flask(__name__)
 
 from commontools import log
 
+#Setup listen socket
+HOST = ''                 # Symbolic name meaning all available interfaces
+PORT = 8082              # Arbitrary non-privileged port
+listener = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+listener.bind((HOST, PORT))
+listener.listen(1)
+lisconn, adr = listener.accept()
+while not lisconn:
+	lisconn, adr = listener.accept()
+
+#Setup sending socket
+#HOST = '0.0.0.0'    # The remote host
+#PORT = 8083           # The same port as used by the server
+#sender = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#sender.connect((HOST, PORT))
+
 #-----------------------------------
 @app.route('/', methods=['POST', 'GET'])
 def index():    
-    if request.method == 'POST':
-        if request.form['submit'] == 'Ping':
-            print('\n')
-            print '***************** PINGING ******************'
-            print('\n')
-            ser = serial.Serial('COM1')
-            ser.write('PING')
-            print 'Pinged'
-            return render_template('index.html')
-        elif request.form['submit'] == 'Pong':
-       		print('\n')
-        	print '***************** WAITING FOR PONG ******************'
-        	print('\n')
-      		serRead = serial.Serial('COM2')
-        	recievedText = serRead.read(4)
-        	print recievedText
-        	print '******* PONGED ********'
-        	return render_template('index.html')
-    	elif request.form['submit'] == 'Ping-Pong':
-       		print('\n')
-        	print '***************** Ping-Pong ******************'
-        	print('\n')
-       		ser = serial.Serial('COM1')
-       		print 'WRITE PING'
-       		ser.write('PING')
-        	recievedText = ser.read(4)
-        	print recievedText
-        	return 'PING PONG'
-    elif request.method == 'GET':
-        return render_template('index.html')
+	if request.method == 'POST':
+		if request.form['submit'] == 'Ping':
+			print('\n')
+			print '***************** PINGING ******************'
+			print('\n')
+			#sender.sendall('PING')
+			return render_template('index.html')
+		elif request.form['submit'] == 'Pong':
+			print('\n')
+			print '***************** WAITING FOR DATA ******************'
+			print('\n')
+			while True :
+				data = lisconn.recv(1024)
+				if data:
+					return data
+		elif request.form['submit'] == 'Ping-Pong':
+			print('\n')
+			print '***************** Ping-Pong ******************'
+			print('\n')
+			print 'WRITE PING'
+			#sender.sendall('PING')
+			while 1:
+				data = lisconn.recv(1024)
+				if data:
+					return data
+			print recievedText
+			return 'PING PONG'
+	elif request.method == 'GET':
+		return render_template('index.html')
 
 #-----------------------------------
 @app.errorhandler(500)
@@ -58,3 +73,4 @@ def internal_error(error):
 @app.errorhandler(404)
 def not_found(error):
 	return make_response(jsonify( { 'error': 'Not found' } ), 404)
+
